b1dcbff542e400d75e8b547eac8432b7310c07a7
BastienJacoud <bastien.jacoud@etu.univ-lyon1.fr>
2018-03-03 00:56:19 +0100

    Avancement FRONT + fenetre de connexion + modif interface + gestion des evenements

--- Client/src/Controller/Controller_Client.java
+++ Client/src/Controller/Controller_Client.java
@@ -1,13 +1,24 @@
 package Controller;
 
 import Main.Main_Client;
+import Uilities.TestRegex;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
+import java.awt.event.MouseEvent;
+import java.util.Optional;
+
 public class Controller_Client {
+
+    //Récupération du tabPane
+    @FXML
+    private TabPane _tabPane;
 
     //Récupération des objets relatifs à la réception de mail
     @FXML
@@ -15,6 +26,12 @@ public class Controller_Client {
 
     @FXML
     private Pagination _pagination;
+
+    @FXML
+    private Button _btnRepondre;
+
+    @FXML
+    private Button _btnSuppr;
 
 
     //Récupération des objets relatifs à l'envoi de mails
@@ -30,47 +47,102 @@ public class Controller_Client {
     @FXML
     private TextArea _tfContenu;
 
+    @FXML
+    private Text _txtMailEmetteur;
+
+    @FXML
+    private Button _btnDeconnexion;
+
+
     private Main_Client _mainClient;
 
+    /*
+    Constructeur
+     */
     public Controller_Client(){
 
     }
+
+    /*
+    Nombre de messages par page
+     */
     private int itemsPerPage() {
         return 10;
     }
 
-
-    public VBox createPage(int pageIndex) {
-        VBox box = new VBox(5);
+    /*
+    Crée une nouvelle page
+     */
+    private VBox createPage(int pageIndex) {
+        VBox box = new VBox(25);
         int page = pageIndex * itemsPerPage();
+        //String headerMail = "";
         for (int i = page; i < page + itemsPerPage(); i++) {
-            VBox element = new VBox();
-            Hyperlink link = new Hyperlink("Item " + (i + 1));
-            link.setVisited(true);
-            Label text = new Label("Search results\nfor " + link.getText());
-            element.getChildren().addAll(link, text);
+            HBox element = new HBox();
+            final int indMsg;
+
+            Hyperlink link = new Hyperlink("Message " + (i + 1));
+            //link.setVisited(true);
+            Label destinataire = new Label("desinataire@mail.fr");
+            Label objet = new Label("fakeObjet");
+            Text contenu = new Text((i+1) + " " + (i+1) +" " + (i+1) + "\n" + (i+1) + " " + (i+1) + "\n" + (i+1));
+
+            element.getChildren().addAll(link, destinataire, objet);
+
+            element.setSpacing(25);
             box.getChildren().add(element);
+
+            indMsg = i +1;
+            link.setOnMouseClicked(MouseEvent -> updateTF(indMsg, destinataire.getText(), objet.getText(), contenu));
+
+
         }
         return box;
     }
 
+    /*
+    Initialise la fenêtre
+     */
     @FXML
     private void initialize(){
-
+        _txtMailEmetteur.setText("test@gmail.com");
         _pagination.setPageFactory((Integer pageIndex) -> createPage(pageIndex));
     }
 
-    private void EnvoiMail(){
+    /*
+    Envoi un mail lors du clic sur le bouton correspondant
+     */
+    private void TestEnvoiMail(){
         Platform.runLater(() ->{
             String destinataire;
             String objet;
             String contenu;
+            destinataire = _tfDestinataire.getText();
+            objet = _tfObjet.getText();
+            contenu = _tfContenu.getText();
 
-            if(!_tfDestinataire.getText().equals("")){
-                destinataire = _tfDestinataire.getText();
-                objet = _tfObjet.getText();
-                contenu = _tfContenu.getText();
-                //Envoi du message
+            if(TestRegex.CheckMail(destinataire)){
+
+                if(!objet.equals(""))
+                {
+                    EnvoiMail(destinataire, objet, contenu);
+                }
+                else {
+                    Alert confirm = new Alert(Alert.AlertType.CONFIRMATION);
+                    confirm.setTitle("Pas d'objet !");
+                    confirm.setContentText("Etes-vous sûr de vouloir envoyer le mail sans objet ?");
+                    ButtonType btnOui = new ButtonType("Oui");
+                    ButtonType btnNon = new ButtonType("Non");
+                    confirm.getButtonTypes().setAll(btnOui, btnNon);
+                    Optional<ButtonType> resultat = confirm.showAndWait();
+
+                    if(resultat.get() == btnOui){
+                        EnvoiMail(destinataire, objet, contenu);
+                    }
+                    else
+                        confirm.close();
+                }
+
             }
             else
             {
@@ -84,9 +156,72 @@ public class Controller_Client {
         });
     }
 
+    private void EnvoiMail(String destinataire,String objet,String contenu){
+        //Envoi du message
+        _mainClient.EnvoiMail(destinataire, objet, contenu);
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Envoi réussi !");
+        alert.setContentText("Message envoyé avec succès.");
+        alert.show();
+
+    }
+
+    /*
+    Mettre à jour le texteFlow avec le contenu des messages
+     */
+    private void updateTF(int ind, String destinataire, String objet, Text contenu){
+        Platform.runLater(() -> {
+            //Mise à jour tu textFlow
+            _txtFlow.getChildren().clear();
+            _txtFlow.getChildren().add(contenu);
+
+            //On met à jour les boutons uniquement si un message a été sélectionné
+            updateBTN(ind, destinataire, objet, contenu.getText());
+        });
+
+    }
+
+    private void updateBTN(int ind, String destinataire, String objet, String contenu){
+        Platform.runLater(() -> {
+            _btnRepondre.setDisable(false);
+            _btnSuppr.setDisable(false);
+            _btnRepondre.setOnMouseClicked(mouseEvent -> RepondreMail(destinataire, objet, contenu));
+            _btnSuppr.setOnMouseClicked(mouseEvent -> SupprMail(ind));
+        });
+
+    }
+
+    private void SupprMail(int ind){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Suppression message !");
+        alert.setContentText("Etes-vous sûr de vouloir supprimer le mail " + ind + " ?");
+        ButtonType btnOui = new ButtonType("Oui");
+        ButtonType btnNon = new ButtonType("Non");
+        alert.getButtonTypes().setAll(btnOui, btnNon);
+        Optional<ButtonType> resultat = alert.showAndWait();
+
+        if(resultat.get() == btnOui)
+            _mainClient.SupprMail(ind);
+        else
+            alert.close();
+    }
+
+    private void RepondreMail(String destinataire, String objet, String contenu){
+        _tabPane.getSelectionModel().select(1);
+        _tfDestinataire.setText(destinataire);
+        objet = "Re : " + objet;
+        _tfObjet.setText(objet);
+        contenu = "\n\n\n\n\n========== Ancien message ==========\n\n" + contenu;
+        _tfContenu.setText(contenu);
+    }
+
+    /*
+    Synchronisation du main avec le controlleur
+     */
     public void SetMain(Main_Client mainClient)
     {
         _mainClient = mainClient;
-        _btnEnvoi.setOnMouseClicked(mouseEvent -> EnvoiMail());
+        _btnEnvoi.setOnMouseClicked(mouseEvent -> TestEnvoiMail());
     }
 }
