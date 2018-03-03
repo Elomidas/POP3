8b274d11ae39a38b64c6896067f57413de47b4f6
Elomidas <victor96@hotmail.fr>
2018-03-03 17:10:17 +0100

    Class Mailbox upgraded
    Project installed on new computer

--- Client/src/Model/MailBox/Mailbox.java
+++ Client/src/Model/MailBox/Mailbox.java
@@ -1,6 +1,7 @@
 package Model.MailBox;
 
 import Model.Protocols.POP3.POP3;
+import Model.Protocols.POP3.POP3Exception;
 import Uilities.TestRegex;
 
 import java.util.ArrayList;
@@ -16,6 +17,16 @@ public class Mailbox {
         m_pop = null;
     }
 
+    public boolean joinServer(String address, int port) throws MailException {
+        m_pop = new POP3();
+        try {
+            m_pop.Connect(address, port);
+        } catch(POP3Exception e) {
+            throw new MailException("Your configuration " + address + ":" + port + " seems invalid...", e);
+        }
+        return this.ServerJoined();
+    }
+
     public void setUser(String user) throws MailException {
         if(TestRegex.CheckMail(user) == false) {
             throw new MailException(user + " isn't a correct mail address.");
@@ -23,12 +34,28 @@ public class Mailbox {
         m_user = user;
     }
 
-    public boolean Authenticate(String password) {
-        //
+    public boolean Authenticate(String password) throws MailException {
+        if(this.ServerJoined() == false) {
+            throw new MailException("You should try to join the server before trying to authenticate yourself.");
+        }
+        boolean result = false;
+        try {
+            m_pop.Authentication(m_user, password);
+        } catch(POP3Exception e) {
+            throw new MailException("Unable to authenticate user '" + m_user + "'", e);
+        }
+        return false;
+    }
+
+    public boolean ServerJoined() {
+        if(m_pop == null) {
+            return false;
+        }
+        return (m_pop.Status() != POP3._DISCONNECTED);
     }
 
     public boolean Usable() {
-        if(m_pop == null) {
+        if(this.ServerJoined() == false) {
             return false;
         }
         return m_pop.CheckConnected();
@@ -45,6 +72,15 @@ public class Mailbox {
         } catch(MailException e) {
             throw e;
         }
+    }
+
+    public int getSize() {
+
+        return m_mails.size();
+    }
+
+    public ArrayList<Mail> getMails() {
+        return m_mails;
     }
 
     public void SendMail(String to, String object, String message) throws MailException {
