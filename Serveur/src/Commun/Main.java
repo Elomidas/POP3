50438f883da6063ecda025ed2bea566675728457
Martial Tardy <tardy.m02@gmail.com>
2018-03-21 15:29:28 +0100

    Reorganisation avec package 2

--- Serveur/src/Commun/Main.java
+++ Serveur/src/Commun/Main.java
@@ -1,11 +1,11 @@
-package SMTP;
+package Commun;
 
+import Commun.Tcp;
 
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-
     final static int port = 1210;
 
     public static void main(String[] args) {
@@ -14,8 +14,8 @@ public class Main {
             System.out.println("Lancement du serveur sur le port " + port);
             while (true){
                 Socket socketClient = serverSocket.accept();
-                Smtp smtp = new Smtp(socketClient);
-                smtp.start();
+                Tcp t = new Tcp(socketClient);
+                t.start();
             }
         }catch(Exception e){
             e.printStackTrace();
